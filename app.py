553782b5998354493d394c5b0bf75450adc066ae
Baligh Mnassri <baligh.mnassri@gmail.com>
2020-09-28 19:27:02 +0200

    new updates

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import spacy
 # loading the model
 nlp = spacy.load("revealing_skills_model")
 
+import fr_core_news_md
+nlpfr = fr_core_news_md.load()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,12 +23,24 @@ def revealing():
 		if len(doc.ents)==0:
 			return render_template("index.html", rawtext=rawtext, message="Pas de compétences révélées dans cet exemple !")
 		else:
+			'''
 			for ent in doc.ents:
 				d.append((ent.label_, ent.text))
 				df = pd.DataFrame(d, columns=('named entity', 'output'))
 				SKILLS_named_entity = df.loc[df['named entity'] == 'Compétences']['output']
 				results = SKILLS_named_entity
-				num_of_results = len(results)           
+				num_of_results = len(results) 
+			'''
+			for ent in doc.ents:
+				if nlpfr(ent.text)[0].pos_ == "VERB":
+					entmod = " ".join([str(nlpfr(ent.text)[0].lemma_)]+[str(token) for token in nlpfr(ent.text)[1:]])
+					d.append((ent.label_, entmod.capitalize()))
+				else:
+					d.append((ent.label_, ent.text.capitalize()))
+				df = pd.DataFrame(d, columns=('named entity', 'output'))
+				SKILLS_named_entity = df.loc[df['named entity'] == 'Compétences']['output']
+				results = SKILLS_named_entity
+				num_of_results = len(results)  
 			return render_template("index.html", rawtext=rawtext, results=results, num_of_results=num_of_results)
 
 if __name__ == '__main__':
