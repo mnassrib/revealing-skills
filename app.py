b3a06eafcc71de4c1f6a67ce2e3be68cafb0a0e5
Baligh Mnassri <baligh.mnassri@gmail.com>
2020-10-06 01:28:36 +0200

    updating the app

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import spacy
 # loading the model
 nlp = spacy.load("revealing_skills_model")
 
-import fr_core_news_sm
-nlpfr = fr_core_news_sm.load()
+#import fr_core_news_sm
+#nlpfr = fr_core_news_sm.load()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("index.html", skills_entities={})
 
 @app.route('/revealing', methods=["POST"])
 def revealing():
@@ -21,19 +21,22 @@ def revealing():
 		doc = nlp(rawtext)
 		d = []
 		if len(doc.ents)==0:
-			return render_template("index.html", rawtext=rawtext, message="Pas de compétences révélées dans cet exemple !")
+			return render_template("index.html", rawtext=rawtext, message="Pas de compétences révélées dans ce texte !")
 		else:
 			for ent in doc.ents:
-				if nlpfr(ent.text)[0].pos_ == "VERB":
-					entmod = " ".join([str(nlpfr(ent.text)[0].lemma_)]+[str(token) for token in nlpfr(ent.text)[1:]])
+				if nlp(ent.text)[0].pos_ == "VERB":
+					entmod = " ".join([str(nlp(ent.text)[0].lemma_)]+[str(token) for token in nlp(ent.text)[1:]])
 					d.append((ent.label_, entmod.capitalize()))
 				else:
 					d.append((ent.label_, ent.text.capitalize()))
-				df = pd.DataFrame(d, columns=('named entity', 'output'))
-				SKILLS_named_entity = df.loc[df['named entity'] == 'Compétences']['output']
-				results = SKILLS_named_entity
-				num_of_results = len(results)  
-			return render_template("index.html", rawtext=rawtext, results=results, num_of_results=num_of_results)
+				skills_entities = get_skills_entities(pd.DataFrame(d, columns=('named entity', 'output'))) 
+			return render_template("index.html", rawtext=rawtext, skills_entities=skills_entities)
 
+def get_skills_entities(df):
+	skills_entity = {}
+	for entit in pd.unique(df["named entity"]):
+		skills_entity[entit] = df[df['named entity'] == entit]['output'].tolist()
+	return skills_entity
+	
 if __name__ == '__main__':
 	app.run(debug=True)
